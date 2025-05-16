227f54a4bff0e018e21f12a5dbd6bb721399bc79
Ani Chinta <anirudhchinta03@gmail.com>
2025-05-16 05:19:09 -0400

    set base for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: "/secret/", // 👈 Add this line for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)), // ✅
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
